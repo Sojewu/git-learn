73314541de59645623f913d16cbfae35d0495bd7
Sojewu <wub66811@gmail.com>
2026-05-22 22:19:58 +0800

    complet file

---
+++ sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+
+void sort(int *arr, int len)
+{
+    int i, j, temp;
+    for (i = 0; i < len - 1; i++)
+    {
+        int flag = 0;
+        for (j = 0; j < len - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                flag = 1;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        if (flag == 0)
+        {
+            break;
+        }
+    }
+}
