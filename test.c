71f74218a9e89291c0daa429fd186448ffb54dea
Wu_ <minshanszu@outlook.com>
2026-05-22 13:20:37 +0800

    added a new functionn

--- test.c
+++ test.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include "test.h"
 
+void test()
+{
+    int a;
+    printf("test\n");
+    scanf("%d", &a);
+    printf("%d\n", a);
+}
+
+
 
 int main()
 {
     printf("hello world\n");
+    test();
+
     return 0;
 }
